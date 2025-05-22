6c9771fc19f8336420bb68fb2f7d5be8f6933ddf
kosiniii <defensiv2010@gmail.com>
2025-05-23 00:55:10 +0300

    fix

--- kos_Htools/__init__.py
+++ kos_Htools/__init__.py
@@ -1,7 +1,7 @@
 """
 kos_Htools - Библиотека инструментов для работы с Telegram и Redis
 """
-from tools import VERSION
+from ..tools import VERSION
 from .telethon_core.clients import MultiAccountManager
 from .telethon_core.settings import TelegramAPI
 from .redis_core.redisetup import RedisBase
