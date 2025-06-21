d6371344da8b696580c73b47abd96a822775c9fe
kosiniii <defensiv2010@gmail.com>
2025-06-21 23:34:25 +0300

    update redisbase

--- kos_Htools/redis_core/redisetup.py
+++ kos_Htools/redis_core/redisetup.py
@@ -1,39 +1,41 @@
 import json
 import logging
-from typing import Union, Dict, List
+from typing import Dict, List
 from redis import Redis
 
 logger = logging.getLogger(__name__)
 
 class RedisBase:
-    def __init__(self, key: str, data: Union[Dict, List], redis: Redis = Redis()):
-        self.redis = redis
+    def __init__(
+            self, 
+            key: str, 
+            data: Dict | List, 
+            redis_client: Redis = None,
+        ):
+        self.redis = redis_client or Redis()
         self.key = key
         self.data = data
     
-    def cached(self, data: Union[Dict, List], key: str | None = None, ex: int | None = None) -> None:   
-        key = key or self.key
-        
+    def cached(self, data: Dict | List, ex: int | None = None) -> None:   
         if isinstance(data, (dict, list)):
             data = json.dumps(data)
             
         try:
-            self.redis.set(name=key, value=data, ex=ex)
+            self.redis.set(name=self.key, value=data, ex=ex)
         except Exception as e:
             logger.error(f'Ошибка в redis: {e}')
     
-    def get_default_value(self, data_type: type) -> Union[Dict, List]:
+    def get_default_value(self, data_type: type) -> Dict | List:
         return {} if data_type == dict else []
     
-    def get_cached(self, key: str | None = None, data_type: type | None = None) -> Union[Dict, List, str, bytes]:
-        key = key or self.key
+    def get_cached(self, data_type: type | None = None) -> Dict | List | str | bytes:
         data_type = data_type or type(self.data)
         result = self.get_default_value(data_type)
 
         try:
-            cached_data = self.redis.get(key)
+            cached_data = self.redis.get(self.key)
             if not cached_data:
-                logger.warning(f'Ключ не найден: {key}')
+                logger.warning(f'Ключ не найден: {self.key}')
                 return result
 
             decode_data = cached_data.decode('utf-8')
@@ -41,7 +43,7 @@ class RedisBase:
                 try:
                     return json.loads(decode_data)
                 except json.JSONDecodeError:
-                    logger.error(f'Ошибка декодирования JSON для ключа: {key}')
+                    logger.error(f'Ошибка декодирования JSON для ключа: {self.key}')
                     return result
             else:
                 return decode_data
@@ -50,10 +52,28 @@ class RedisBase:
             logger.error(f'Ошибка получения данных: {e}')
             return result   
 
-    def delete_key(self, key: str | None = None) -> None:
-        key = key or self.key
+    def delete_key(self) -> None:
         try:
-            self.redis.delete(key)
+            self.redis.delete(self.key)
         except Exception as e:
-            logger.error(f'Ошибка удаления ключа {key}: {e}')
+            logger.error(f'Ошибка удаления ключа {self.key}: {e}')
 
+
+class RedisShortened(RedisBase):
+    def lpush(self, *values):
+        return self.redis.lpush(self.key, *values)
+
+    def rpush(self, *values):
+        return self.redis.rpush(self.key, *values)
+
+    def lpop(self):
+        return self.redis.lpop(self.key)
+
+    def rpop(self):
+        return self.redis.rpop(self.key)
+
+    def lrange(self, start, end):
+        return self.redis.lrange(self.key, start, end)
+
+    def llen(self):
+        return self.redis.llen(self.key)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, "README.md"), "r", encoding="utf-8") as fh:
 
 setup(
     name="kos_Htools",
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[
         "telethon>=1.39.0",
